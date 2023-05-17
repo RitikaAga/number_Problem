40f92cf429c06e15a65770890b415c9107c78b8b
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2023-05-17 18:23:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+const numberInput = document.getElementById('numberInput');
+const message = document.getElementById('message');
+
+numberInput.addEventListener('input', handleNumberInput);
+
+function handleNumberInput() {
+  const value = parseInt(numberInput.value);
+
+  if (isNaN(value)) {
+    message.textContent = 'Please enter a valid number.';
+    return;
+  }
+
+  if (value < 0) {
+    message.textContent = 'Please enter a positive value.';
+    return;
+  }
+
+  message.textContent = '';
+
+  if (value % 2 === 0) {
+    displayNextEvenNumbers(value);
+  } else {
+    displayNextOddNumbers(value);
+  }
+}
+
+function displayNextEvenNumbers(number) {
+  const nextEvenNumbers = [number + 2, number + 4, number + 6];
+  message.textContent = 'Next 3 even numbers: ' + nextEvenNumbers.join(', ');
+}
+
+function displayNextOddNumbers(number) {
+  const nextOddNumbers = [number + 2, number + 4, number + 6];
+  message.textContent = 'Next 3 odd numbers: ' + nextOddNumbers.join(', ');
+}
